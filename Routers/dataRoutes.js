9c42b1504a8429bf484be89bfe7ad493f5325ef5
Abhinav <abhinavgtechnical@gmail.com>
2023-12-29 19:34:16 +0530

    Improved Order API

--- Routers/dataRoutes.js
+++ Routers/dataRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { shoesList } from "../controllers/dataControl.js";
-import { createOrders, getOrders } from "../controllers/orderControl.js";
+import {
+  clearOrders,
+  createOrders,
+  getOrders,
+} from "../controllers/orderControl.js";
 
 const dataRoutes = express.Router();
 
 dataRoutes.get("/api_items", shoesList);
-dataRoutes.get("/orders", getOrders);
 dataRoutes.post("/orders", createOrders);
+dataRoutes.post("/getOrders", getOrders);
+dataRoutes.post("/clearOrders", clearOrders);
 
 export default dataRoutes;
--- controllers/orderControl.js
+++ controllers/orderControl.js
@@ -12,19 +12,41 @@ export const createOrders = async (req, res) => {
       userId,
     });
     await order.save();
-    res.status(201).json(order);
+    return res.status(201).json(order);
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    return res.status(500).send("Internal Server Error");
   }
 };
 
 export const getOrders = async (req, res) => {
+  const { userId } = req.body;
   try {
-    const orders = await Order.find().populate("user", "email");
-    res.json(orders);
+    const orders = await Order.find({ userId });
+    let sortedData;
+    orders.length > 0
+      ? (sortedData = orders.sort(
+          (a, b) => new Date(a.date) - new Date(b.date)
+        ))
+      : (sortedData = []);
+
+    return res.json(sortedData);
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const clearOrders = async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const orders = await Order.find({ userId });
+    await Order.deleteMany({ userId });
+    const delOrders = await Order.find({ userId });
+
+    return res.json(delOrders);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
   }
 };
